805c4b7d245d9e20f57e32518fb7200266852c35
Your Name <kboothe617@gmail.com>
2024-12-04 13:11:39 -0700

    copied develope folder over because I remembered I could do that

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// import vite and react
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    envDir: './env',
-    plugins: [react()],
-})
+  envDir: './env',
+  plugins: [react()],
+});
