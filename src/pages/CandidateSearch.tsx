034c59066d56ff4b794da0fa419e3e629151d549
Your Name <kboothe617@gmail.com>
2024-12-04 13:50:49 -0700

    const search for users

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -4,7 +4,7 @@ import { Candidate } from '../interfaces/Candidate.interface';
 
 
 const CandidateSearch = () => {
-  const [results, setResxults] = useState<Candidate[]>([]);
+  const [results, setResults] = useState<Candidate[]>([]);
   const [currentUser, setCurrentUser] = useState<Candidate>({
     id: null,
     login: null,
@@ -18,9 +18,16 @@ const CandidateSearch = () => {
   });
 
   const [currentIdx, setCurrentIdx] = useState<number>(0);
+
   const searchForSpecificUser = async (user: string) => {
     const data: Candidate = await searchGithubUser(user);
     setCurrentUser(data);
+  };
+
+  const searchForUsers = async (query: string) => {
+    const data: Candidate[] = await searchGithub();
+    setResults(data);
+    await searchForSpecificUser(data[currentIdx].login || '');
   };
 
   return <h1>CandidateSearch</h1>;
