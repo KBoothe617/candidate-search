eeb081af2c56fed55a7efe8a2dec4b3d0a2fe36d
Your Name <kboothe617@gmail.com>
2024-12-04 13:21:17 -0700

    nav.tsx navtab class and navitem class link to home

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,9 +2,15 @@
 import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+  const currentPage = useLocation().pathname;
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <ul className='nav-tabs'>
+      <li className='nav-item'>
+        <Link to='/' className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
+        
+      </li>
+    </ul>
   )
 };
 
