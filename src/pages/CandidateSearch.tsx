68f0d9b618dc4481146cddeb294ca57ac35655d2
Your Name <kboothe617@gmail.com>
2024-12-04 13:43:19 -0700

    import candidat from interfaces, and const usestates

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,22 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+
 
 const CandidateSearch = () => {
+  const [results, setResxults] = useState<Candidate[]>([]);
+  const [currentUser, setCurrentUser] = useState<Candidate>({
+    id: null,
+    login: null,
+    email: null,
+    html_url: null,
+    avatar_url: null,
+    name: null,
+    location: null,
+    bio: null,
+    company: null,
+  });
+
   return <h1>CandidateSearch</h1>;
 };
 
