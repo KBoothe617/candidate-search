253c3ad88a8b4f01a9aedd790bb1291e41662f43
Your Name <kboothe617@gmail.com>
2024-12-05 12:17:46 -0700

    SavedCandidateList.tsx import candidate interface SavedCandidate and react

---
+++ src/components/SavedCandidateList.tsx
@@ -0,0 +1,5 @@
+// import candidate interface SavedCandidate and react
+import { Candidate } from "../interfaces/Candidate.interface";
+import SavedCandidate from "./SavedCandidate";
+import { useEffect, useState } from "react";
+
